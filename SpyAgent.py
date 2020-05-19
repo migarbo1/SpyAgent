45bb0c882ba18cc53a51e88bde3e6a0db37aa72c
Miguel <migarbo1@inf.upv.es>
2020-05-19 13:20:26 +0200

    spy agent creation completed. SpyUsers behaviour on_start method completed. tun and at_end remaining

--- SpyAgent.py
+++ SpyAgent.py
@@ -5,10 +5,10 @@ import random
 import requests
 
 
-# auxiliar methods
+# auxiliary methods
 
 def unique(list1):
-    # intilize a null list
+    # intialize a null list
     unique_list = []
     # traverse for all elements
     for x in list1:
@@ -25,24 +25,74 @@ class SpyAgent(Agent):
         super().__init__(jid, password)
         self.guid = guid
         self.agName = name
-        self.users_contacted = {}                   # dictionary k = guid, v = num messages sent
-        self.friends = {}                           # array containing the guid of the agent friends
-        self.user_by_theme = {}                     # dictionary k = theme, v = (user guid, selection)
+        self.users_contacted = {}  # dictionary k = guid, v = (theme, num messages sent)
+        self.friends = []  # array containing the guid of the agent friends
+        self.user_by_theme = {}  # dictionary k = theme, v = (user guid, selection)
 
     async def setup(self):
-        beh = self.SpyUsers(period=900)             # every 15 minutes
+        beh = self.SpyUsers(period=900)  # every 15 minutes
         self.add_behaviour(beh)
+        print('agent ready')
 
     class SpyUsers(PeriodicBehaviour):
-        async def on_start(self):                   # get_agent_friends(), select_users()
+        async def on_start(self):  # get_agent_friends(), select_users()
+            print('first, getting friends')
+            res = requests.get('http://localhost/services/api/rest/json/?',
+                               params={'method': 'users.get_agent_friends',
+                                       'agentGUID': spy.guid}
+                               )
+            if res:
+                print('friends response received')
+                content = res.json()
+                if content['status'] == 0:
+                    print('friends status good')
+                    content = content['result']
+                    for k in content.keys():
+                        spy.friends += [int(content[k])]
+                    print('friends added')
+                    initial_friends = spy.friends
+                else:
+                    print('status incorrect')
+                    print(content)
+            else:
+                print('res failed')
+                print(res.json())
+
+            print('now getting users information')
+            for i in random.shuffle([0, 1, 2, 3, 4, 5]):
+                res = requests.get('http://localhost/services/api/rest/json/?',
+                                   params={'method': 'users.select_users',
+                                           'theme': i,
+                                           'agentGUID': spy.guid}
+                                   )
+                if res:
+                    print('users information response received')
+                    content = res.json()
+                    if content['status'] == 0:
+                        print('users information status good')
+                        content = content['result']
+                        if user_by_theme.get(i, 0) == 0:
+                            spy.user_by_theme[i] = []
+
+                        for k in content.keys():                    # for each user returned
+                            tup = (int(k), content[k])
+                            spy.user_by_theme[i] += [tup]           # inserts to the theme, (guid, selection)
+                            if spy.users_contacted.get(int(k),0) == 0 and int(k) not in spy.friends:        # if the user hasn't been processed yet and it isn't a friend
+                                spy.users_contacted[int(k)] = (i, 0)    # save the user to be contacted later
+                        print('users to be contacted and users indexed by theme correctly created')
+                    else:
+                        print('status ' + str(i) + ' incorrect')
+                        print(content)
+                else:
+                    print('res ' + str(i) + ' failed')
+                    print(res.json())
+            print('start behaviour finished')
+
+        async def run(self):  # send_message()
             return
 
-        async def run(self):                        # send_message()
+        async def at_end(self):  # report_info()
             return
-
-        async def at_end(self):                     # report_info()
-            return
-
 
 
 # main
